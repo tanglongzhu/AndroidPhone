a1c0cf052fb70aa7a845874ad7e78e50d13089b3
XC-AE-0817-C\Administrator <tangjin2012@189.cn>
2021-11-29 16:08:44 +0800

    添加新安卓原版的源码

---
+++ FullscreenApp/app/src/main/java/com/wanbangauto/fullscreenapp/HelloTest.java
@@ -0,0 +1,35 @@
+package com.wanbangauto.fullscreenapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloTest {
+    private int one;
+    private int three;
+    String two;
+    private final ArrayList<String> mNameList;
+
+    public HelloTest(int one) {
+        this.one = one;
+        new MainActivity.TestPer.Subone(){};
+
+        if (two==null) {
+
+        }
+
+        mNameList = new ArrayList<>();
+
+
+        while (one > 10) {
+
+        }
+        for (String name : mNameList) {
+
+
+        }
+
+
+
+    }
+
+}
---
+++ FullscreenApp/app/src/main/java/com/wanbangauto/fullscreenapp/MainActivity.java
@@ -0,0 +1,73 @@
+package com.wanbangauto.fullscreenapp;
+
+import android.content.Intent;
+import android.content.UriMatcher;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.wanbangauto.fullscreenapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    private final UriMatcher mUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    public interface TestPer{
+        void onClick();
+
+        public interface Subone {
+
+        }
+
+    }
+
+
+}
